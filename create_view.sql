e8b4b3fb53ecfeb865e5c567d6e11e81a4b55720
Daniel Son <sondaniel96@gmail.com>
2016-10-30 11:21:26 -0700

    started creating table slowly

--- create_view.sql
+++ create_view.sql
@@ -1,1 +1,20 @@
 #SQL file to create tables
+CREATE TABLE IF NOT EXISTS OwnedVehicle (
+	VIN int,
+	DateOwned Date,
+	TotalMiles int,
+	YearlyMileage int,
+	RegisteredDate Date,
+	LicenseNumber varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS VehicleCatalog (
+	Make varchar(30),
+	Model varchar(30)
+);
+
+CREATE TABLE IF NOT EXISTS RepairOrder (
+	DateOrdered Date,
+	RepairDate Date
+);
+
