fa43a9e2f3b3fef85b0a4de8c09974d5fd28077e
Daniel Son <sondaniel96@gmail.com>
2016-11-28 00:27:33 -0800

    creating tables

--- create_view.sql
+++ create_view.sql
@@ -1,16 +1,26 @@
 #SQL file to create tables
+CREATE TABLE IF NOT EXISTS Customer (
+	CustomerID int not null primary key AUTO_INCREMENT,
+	Phone varchar(11),
+	loyaltyPoints #need to fix in relational
+	Email varchar(30),
+
+);
+
 CREATE TABLE IF NOT EXISTS OwnedVehicle (
-	VIN int,
+	VinNumber varchar(20) not null primary key,
 	DateOwned Date,
 	TotalMiles int,
 	YearlyMileage int,
 	RegisteredDate Date,
-	LicenseNumber varchar(20)
+	LicenseNumber varchar(20),
+	CustomerID int not
 );
 
 CREATE TABLE IF NOT EXISTS VehicleCatalog (
-	Make varchar(30),
-	Model varchar(30)
+	Make varchar(30) not null primary key,
+	Model varchar(30) not null primary key,
+	Year Year(4) not null primary key
 );
 
 CREATE TABLE IF NOT EXISTS RepairOrder (
@@ -18,7 +28,23 @@ CREATE TABLE IF NOT EXISTS RepairOrder (
 	RepairDate Date
 );
 
-CREATE TABLE IF NOT EXISTS Service (
-	action varchar(60),
-	cost float(7,2)
+CREATE TABLE IF NOT EXISTS RepairOrder (
+	RepairOrderID not null primary key AUTO_INCREMENT,
+	DateOrdered datetime,
+	RepairDate datetime,
+	#need total Order,
+	VinNumber int,
+	constraint fk_VehicleRepairOrder foreign key (VinNumber) references OwnedVehicle (VinNumber)
+);
+
+CREATE TABLE IF NOT EXISTS ServiceItem (
+	ServiceitemID not null primary key AUTO_INCREMENT
+);
+
+CREATE TABLE IF NOT EXISTS RepairLine (
+	ServiceitemID not null primary key,
+	RepairOrderID not null primary key,
+	# need subcost
+	constraint fk_ServiceRepairLine foreign key (ServiceitemID) references ServiceItem (ServiceitemID),
+	constraint fk_OrderRepairLine foreign key (RepairOrderID) references RepairOrderID (RepairOrderID)
 );
