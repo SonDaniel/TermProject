b8879bc7649468569e5483a86d8aae0744a44db1
Eugene Mesina <eugenemesina21@gmail.com>
2016-11-30 22:02:44 -0800

    Clean Insert.sql
    merged two inserts alex and mine

--- insert.sql
+++ insert.sql
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
-=======
+
 #The insert statements used to populate the tables
 
 insert into Employee(EmployeeID, EFirstName, ELastName, Phone)
 values (1,'Jimmy','Chao', '1111111111');
 insert into Employee(EmployeeID, EFirstName, ELastName, Phone)
-values (2,'Eugene','Messina', '1111111112');
+values (2,'Eugene','Mesina', '1111111112');
 insert into Employee(EmployeeID, EFirstName, ELastName, Phone)
 values (3,'Alex','Tol', '1111111113');
 insert into Employee(EmployeeID, EFirstName, ELastName, Phone)
@@ -24,31 +23,27 @@ insert into Employee(EmployeeID, EFirstName, ELastName, Phone)
 values (10,'Derik','Derikson', '1111111119');
 
 
->>>>>>> b4f4694c8a9192e3518dc9aed21f7d463e786fa7
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#1
- values(1982,710-562-2011,0,'Bobsherman@hotmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#2
- Values(003,818-667-9479,0,'ealonzo003@gmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#3
- values(004,911-911-9111,0,'about4turtles@gmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#4
- values(21, 010-021-1997,0,'fake@Fakemail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#5
- values(57, 310-210-2879,0,'rocker@hooner.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#6
- values(5, 661-287-2882,0,'about5@gmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#7
- values(109, 182-283-2918,0,'Customer@customermail.com');
-insert into customer(CustomerID,Phone,loyaltyPoints,email)#8
- values(1021,003-007-1021,0,'Fighter@jet.com');
-insert into customer(CustomerID,Phone,loyaltyPoints,email)#9
- values(1291,021-427-1231,0,'Matt@Matt.com');
- insert into customer(CustomerID,Phone,loyaltyPoints,email)#10
-<<<<<<< HEAD
- values(1287,2153-237-2411,0,'Damn@Daniel.com');
-=======
- values(1287,2153-237-2411,0,'Damn@Daniel.com');
 
+insert into Customer(CustomerID,Phone,email)#1
+ values(1982,710-562-2011,'Bobsherman@hotmail.com');
+insert into Customer(CustomerID,Phone,email)#2
+ Values(003,818-667-9479,'ealonzo003@gmail.com');
+insert into Customer(CustomerID,Phone,email)#3
+ values(004,911-911-9111,'about4turtles@gmail.com');
+insert into Customer(CustomerID,Phone,email)#4
+ values(21, 010-021-1997,'fake@Fakemail.com');
+insert into Customer(CustomerID,Phone,mail)#5
+ values(57, 310-210-2879,'rocker@hooner.com');
+insert into Customer(CustomerID,Phone,email)#6
+ values(5, 661-287-2882,'about5@gmail.com');
+insert into Customer(CustomerID,Phone,email)#7
+ values(109, 182-283-2918,'Customer@customermail.com');
+insert into customer(CustomerID,Phone,email)#8
+ values(1021,003-007-1021,'Fighter@jet.com');
+insert into customer(CustomerID,Phone,email)#9
+ values(1291,021-427-1231,'Matt@Matt.com');
+ insert into customer(CustomerID,Phone,email)#10
+ values(1287,2153-237-2411,'Damn@Daniel.com'); 
  
  insert into Adress(CustomerID,AddressType,Street,AddressNumber,ZipCode)
  values(1982,'home','Wew St.',124,11111);
@@ -70,4 +65,26 @@ insert into customer(CustomerID,Phone,loyaltyPoints,email)#9
  values(1291,'Coporate HQ','Gold way.',1812,11111);
  insert into Adress(CustomerID,AddressType,Street,AddressNumber,ZipCode)
  values(1287,'Corporate HQ','Silver St',0011,11111);
->>>>>>> b4f4694c8a9192e3518dc9aed21f7d463e786fa7
+ 
+insert into VehicleCatalog(Make,Model, Year)#1
+values('Challenger','Mk2',1998);
+insert into VehicleCatalog(Make,Model, Year)#2
+values('Nissan','370z',2016);
+insert into VehicleCatalog(Make,Model, Year)#3
+values('Subaru','BRZ',2016);
+insert into VehicleCatalog(Make,Model, Year)#4
+values('Mazda','MX-5',2018);
+insert into VehicleCatalog(Make,Model, Year)#5
+values('Mazda','Rx-7',1998);
+insert into VehicleCatalog(Make,Model, Year)#6
+values('Ford','GT',2013);
+insert into VehicleCatalog(Make,Model, Year)#7
+values('Dodge','Hellcat',2015);
+insert into VehicleCatalog(Make,Model, Year)#8
+values('Toyota','AE86',1983);
+insert into VehicleCatalog(Make,Model, Year)#9
+values('Toyota','Supra',1992);
+insert into VehicleCatalog(Make,Model, Year)#10
+values('Acura','NSX',2018);
+insert into VehicleCatalog(Make,Model, Year)#11
+values('DanielSon','CardboardBox',3000);
