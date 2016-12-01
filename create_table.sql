71188dbd227df0523bb8ed2f3c7849b0c70d2e0b
Jimmy <iamjimmyjimmy@yahoo.com>
2016-12-01 11:05:07 -0800

    Tables and Fixed foreign key constraints
    Tables have been ordered by parents to children

--- create_table.sql
+++ create_table.sql
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS EmploymentTime(
 CREATE TABLE IF NOT EXISTS Mechanic(
 	EmploymentField varchar(30),
 	MechanicInstance int not null primary key,
-	constraint fk_Mechanic foreign key (MechanicInstance) references EmployeeTime (EmployeeInstance)
+	constraint fk_Mechanic foreign key (MechanicInstance) references EmploymentTime (EmployeeInstance)
 );
 
 CREATE TABLE IF NOT EXISTS ServiceTechnician(
 	EmploymentField varchar(30),
 	ServiceTechnicianInstance int not null primary key,
-	constraint fk_ServiceTechnician foreign key  (ServiceTechnicianInstance) references EmployeeTime (EmployeeInstance)
+	constraint fk_ServiceTechnician foreign key  (ServiceTechnicianInstance) references EmploymentTime (EmployeeInstance)
 );
 CREATE TABLE IF NOT EXISTS Certificate(
 	CertificateID int not null primary key,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS MentorShip(
 	StartTime datetime,
 	primary key (MenteeInstance,MentorInstance,CertificateID),
     constraint fk_MentorShip foreign key (MentorInstance,CertificateID) references TempCertificate (MechanicInstance,CertificateID),
-    constraint fk_Mentee foreign key(MenteeInstance) references Mechanic (MechanicInstances)
+    constraint fk_Mentee foreign key(MenteeInstance) references Mechanic (MechanicInstance)
 );
 CREATE TABLE IF NOT EXISTS Customer (
 	CustomerID int not null primary key AUTO_INCREMENT,
@@ -65,97 +65,6 @@ CREATE TABLE IF NOT EXISTS Address (
     PRIMARY KEY (customerID,AddressType),
 	constraint fk_CustomerAddress foreign key (CustomerID) references Customer (CustomerID)
 );
-
-CREATE TABLE IF NOT EXISTS VehicleCatalog (
-	Make varchar(30) not null,
-	Model varchar(30) not null,
-	Year Year(4) not null,
-	primary key (Make, Model, Year)
-);
-
-CREATE TABLE IF NOT EXISTS OwnedVehicle (
-	VinNumber varchar(20) not null primary key,
-	DateOwned Date,
-	TotalMiles int,
-	YearlyMileage int,
-	RegisteredDate Date,
-	LicenseNumber varchar(20),
-	CustomerID int,
-	Make varchar(30),
-	Model varchar(30),
-	Year Year(4),
-	constraint fk_CatalogVehicle foreign key (Make, Model, Year) references VehicleCatalog (Make, Model, Year),
-	constraint fk_CustomerVehicle foreign key (CustomerID) references Customer (CustomerID)
-);
-
-CREATE TABLE IF NOT EXISTS RepairOrder (
-	RepairOrderID int not null primary key AUTO_INCREMENT,
-	DateOrdered datetime,
-	RepairDate datetime,
-	#need total Order,
-	VinNumber varchar(20),
-    ProspectiveID int,
-    TechnicianInstance int,
-	constraint fk_VehicleRepairOrder foreign key (VinNumber) references OwnedVehicle (VinNumber),
-	constraint fk_ServiceTechnician foreign key(TechnicianInstance) references ServiceTechnician(ServiceTechnicianInstance)
-);
-
-CREATE TABLE IF NOT EXISTS ServiceItem (
-	ServiceitemID int not null primary key AUTO_INCREMENT
-);
-
-CREATE TABLE IF NOT EXISTS RepairLine (
-	ServiceitemID int not null,
-	RepairOrderID int not null,
-    MechanicInstance int not null,
-	# need subcost
-	primary key (ServiceitemID, RepairOrderID),
-	constraint fk_ServiceRepairLine foreign key (ServiceitemID) references ServiceItem (ServiceitemID),
-	constraint fk_OrderRepairLine foreign key (RepairOrderID) references RepairOrder (RepairOrderID),
-	constraint fk_Mechanic foreign key(MechanicInstance) references Mechanic (MechanicInstance)
-);
-
-CREATE TABLE IF NOT EXISTS MaintenancePackage (
-	ServiceitemID int not null primary key,
-	PackageTitle varchar(20),
-	constraint fk_ServicePackage foreign key (ServiceitemID) references ServiceItem (ServiceitemID)
-);
-
-CREATE TABLE IF NOT EXISTS ServicePackageLine (
-	ServiceitemID int not null,
-	MaintainancePackageID int not null,
-	primary key (ServiceitemID, MaintainancePackageID),
-	constraint fk_PackageLineMaintainancePackage foreign key (MaintainancePackageID) references MaintenancePackage (ServiceitemID),
-	constraint fk_PackageLineService foreign key (ServiceitemID) references ServiceItem (ServiceitemID)
-);
-
-CREATE TABLE IF NOT EXISTS IndividualService (
-	ServiceitemID int not null primary key,
-	Service varchar(30),
-	Cost Decimal(13,2),
-	CertificateNeeded int,
-	constraint fk_ItemIndividualService foreign key (ServiceitemID) references Serviceitem (ServiceitemID)
-);
-
-CREATE TABLE IF NOT EXISTS PartCatalog (
-	PartCatalogID int not null primary key AUTO_INCREMENT,
-	PartName varchar(30),
-	Cost Decimal(13,2)
-);
-
-CREATE TABLE IF NOT EXISTS PartUsage (
-	IndividualServiceID int not null,
-	Make varchar(30) not null,
-	Model varchar(30) not null,
-	Year Year(4) not null,
-	PartCatalogID int not null,
-	Quantity int,
-	primary key (IndividualServiceID, Make, Model, Year, PartCatalogID),
-	constraint fk_IndividualServicePartUsage foreign key (IndividualServiceID) references IndividualService (ServiceitemID),
-	constraint fk_PartCatalogPartUsage foreign key (PartCatalogID) references PartCatalog (PartCatalogID),
-	constraint fk_VehicleCatalogPartUsage foreign key (Make, Model, Year) references VehicleCatalog (Make, Model, Year)
-);
-
 
 CREATE TABLE IF NOT EXISTS Corporation (
 	CustomerID int not null primary key,
@@ -190,14 +99,14 @@ CREATE TABLE IF NOT EXISTS PremiumCustomer (
 
 CREATE TABLE IF NOT EXISTS ProspectiveCustomer (
 	CustomerID int not null primary key,
-	ContractedID int,
+	ContractedID int not null,
 	SpecialPromotion varchar(30),
 	EmailsSent boolean,
 	ReferralCode varchar(20),
 	ReferralAwards varchar(20),
 	referralAwardsUsed boolean,
 	constraint fk_CustomerIDProspective foreign key (CustomerID) references Customer (CustomerID),
-	constraint fk_ConstraintIDContracted foreign key (ContractedID) references Contracted (ContractedID)
+	constraint fk_ConstraintIDContracted foreign key (ContractedID) references Contracted (CustomerID)
 );
 
 CREATE TABLE IF NOT EXISTS MonthlyPayments (
@@ -208,6 +117,98 @@ CREATE TABLE IF NOT EXISTS MonthlyPayments (
 	PaymentMethods varchar(20),
 	MonthlyCost Decimal(13,2),
 	constraint fk_CustomerIDMonthly foreign key (CustomerID) references PremiumCustomer (CustomerID),
-	constraint fk_ProspectiveIDMonthly foreign key (ProspectiveID) references Prospective,
-	constraint uk_MonthlyBill unique (CustomerID, DateBilled)
+	constraint fk_ProspectiveIDMonthly foreign key (ProspectiveID) references ProspectiveCustomer (CustomerID),
+	constraint uk_MonthlyBill unique (ProspectiveID, DateBilled)
 );
+
+CREATE TABLE IF NOT EXISTS VehicleCatalog (
+
+	Make varchar(30) not null,
+	Model varchar(30) not null,
+	Year Year(4) not null,
+	primary key (Make, Model, Year)
+);
+
+CREATE TABLE IF NOT EXISTS OwnedVehicle (
+	VinNumber varchar(20) not null primary key,
+	DateOwned Date,
+	TotalMiles int,
+	YearlyMileage int,
+	RegisteredDate Date,
+	LicenseNumber varchar(20),
+	CustomerID int,
+	Make varchar(30),
+	Model varchar(30),
+	Year Year(4),
+	constraint fk_CatalogVehicle foreign key (Make, Model, Year) references VehicleCatalog (Make, Model, Year),
+	constraint fk_CustomerVehicle foreign key (CustomerID) references Customer (CustomerID)
+);
+
+CREATE TABLE IF NOT EXISTS RepairOrder (
+	RepairOrderID int not null primary key AUTO_INCREMENT,
+	DateOrdered datetime,
+	RepairDate datetime,
+	#need total Order,
+	VinNumbers varchar(20),
+    ProspectiveID int,
+    ServiceTechnicianInstance int,
+	constraint fk_VehicleRepairOrders foreign key (VinNumbers) references OwnedVehicle (VinNumber),
+	constraint fk_ServiceTechnicians foreign key(ServiceTechnicianInstance) references ServiceTechnician(ServiceTechnicianInstance)
+);
+
+CREATE TABLE IF NOT EXISTS ServiceItem (
+	ServiceitemID int not null primary key AUTO_INCREMENT
+);
+
+CREATE TABLE IF NOT EXISTS RepairLine (
+	ServiceitemID int not null,
+	RepairOrderID int not null,
+    MechanicInstance int not null,
+	# need subcost
+	primary key (ServiceitemID, RepairOrderID),
+	constraint fk_ServiceRepairLines foreign key (ServiceitemID) references ServiceItem (ServiceitemID),
+	constraint fk_OrderRepairLines foreign key (RepairOrderID) references RepairOrder (RepairOrderID),
+	constraint fk_Mechanics foreign key(MechanicInstance) references Mechanic (MechanicInstance)
+);
+
+CREATE TABLE IF NOT EXISTS MaintenancePackage (
+	ServiceitemID int not null primary key,
+	PackageTitle varchar(20),
+	constraint fk_ServicePackage foreign key (ServiceitemID) references ServiceItem (ServiceitemID)
+);
+
+CREATE TABLE IF NOT EXISTS ServicePackageLine (
+	ServiceitemID int not null,
+	MaintainancePackageID int not null,
+	primary key (ServiceitemID, MaintainancePackageID),
+	constraint fk_PackageLineMaintainancePackage foreign key (MaintainancePackageID) references MaintenancePackage (ServiceitemID),
+	constraint fk_PackageLineService foreign key (ServiceitemID) references ServiceItem (ServiceitemID)
+);
+
+CREATE TABLE IF NOT EXISTS IndividualService (
+	ServiceitemID int not null primary key,
+	Service varchar(30),
+	Cost Decimal(13,2),
+	CertificateNeeded int,
+	constraint fk_ItemIndividualService foreign key (ServiceitemID) references ServiceItem (ServiceitemID)
+);
+
+CREATE TABLE IF NOT EXISTS PartCatalog (
+	PartCatalogID int not null primary key AUTO_INCREMENT,
+	PartName varchar(30),
+	Cost Decimal(13,2)
+);
+
+CREATE TABLE IF NOT EXISTS PartUsage (
+	IndividualServiceID int not null,
+	Make varchar(30) not null,
+	Model varchar(30) not null,
+	Year Year(4) not null,
+	PartCatalogID int not null,
+	Quantity int,
+	primary key (IndividualServiceID, Make, Model, Year, PartCatalogID),
+	constraint fk_IndividualServicePartUsage foreign key (IndividualServiceID) references IndividualService (ServiceitemID),
+	constraint fk_PartCatalogPartUsage foreign key (PartCatalogID) references PartCatalog (PartCatalogID),
+	constraint fk_VehicleCatalogPartUsage foreign key (Make, Model, Year) references VehicleCatalog (Make, Model, Year)
+);
+
