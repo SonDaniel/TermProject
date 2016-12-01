1258ba1c5f469b7f0675666151e99262b54715d5
Jimmy <iamjimmyjimmy@yahoo.com>
2016-11-30 20:23:30 -0800

    Employee Part Tables
    Finish adding the constraints and foreign keys to the Employee
    EmployeeTime
    Mechanic
    Service Technician
    MentorShip
    TempCertificate
    Certificate

--- create_table.sql
+++ create_table.sql
@@ -105,52 +105,53 @@ CREATE TABLE IF NOT EXISTS Employee (
 	EmployeeID int not null primary key,
 	EFirstName varchar(30),
 	ELastName  varchar(30),
-	Phone int
+	Phone int,
+	Constraint UC_Employee_ID UNIQUE (EFirstName,ELastName,Phone)
 );
 CREATE TABLE IF NOT EXISTS EmploymentTime(
-	DateRetired int not null primary key,
+	EmployeeInstance int not null auto_increment primary key,
+	DateRetired int not null,
 	DateEmployed int,
-	EmployeeID int
+	EmployeeID int,
+    Constraint UC_EmployeeInstance UNIQUE (DateEmployed,EmployeeID),
+    constraint fk_EmployeeID foreign key (EmployeeID) references Employee (EmployeeID)
 );
 
 CREATE TABLE IF NOT EXISTS Mechanic(
 	EmploymentField varchar(30),
-	EmployeeID int not null primary key,
-	constraint fk_Mechanic foreign key (EmployeeID) references Employee (EmployeeID)
-);
-
-CREATE TABLE IF NOT EXISTS Mechanic(
-	EmploymentField varchar(30),
-	EmployeeID int not null primary key,
-	constraint fk_Mechanic foreign key (EmployeeID) references Employee (EmployeeID)
+	MechanicInstance int not null primary key,
+	constraint fk_Mechanic foreign key (MechanicInstance) references EmployeeTime (EmployeeInstance)
 );
 
 CREATE TABLE IF NOT EXISTS ServiceTechnician(
 	EmploymentField varchar(30),
-	EmployeeID int not null primary key,
-	constraint fk_ServiceTechnician foreign key (EmployeeID) references Employee (EmployeeID)
+	ServiceTechnicianInstance int not null primary key,
+	constraint fk_ServiceTechnician foreign key  (ServiceTechnicianInstance) references EmployeeTime (EmployeeInstance)
 );
-
-CREATE TABLE IF NOT EXISTS MentorShip(
-	EmployeeID int not null,
-	MentorID int not null,
-	Skill varchar(30) not null,
-	Stop datetime,
-	Start datetime,
-	primary key (EmployeeID, MentorID, Skill)
-);
-CREATE TABLE IF NOT EXISTS TempCertificate(
-	EmployeeID int not null,
-	CertificateID int not null,
-	Skill int not null,
-	CertificateName varchar(30),
-	primary key (EmployeeID, CertificateID, Skill)
-);
-
 CREATE TABLE IF NOT EXISTS Certificate(
 	CertificateID int not null primary key,
 	CertificateLevel int,
 	ServiceType varchar(30) not null
 );
+CREATE TABLE IF NOT EXISTS TempCertificate(
+	MechanicInstance int not null,
+	CertificateID int not null,
+	CertificateName varchar(30),
+	primary key (MechanicInstance, CertificateID),
+    constraint fk_TempCertificate foreign key(MechanicInstance) references Mechanic (MechanicInstance),
+    constraint fk_Certificate foreign key(CertificateID) references Certificate (CertificateID)
+);
+CREATE TABLE IF NOT EXISTS MentorShip(
+	MenteeInstance int not null,
+	MentorInstance int not null,
+	CertificateID int not null,
+	StopTime datetime,
+	StartTime datetime,
+	primary key (MenteeInstance,MentorInstance,CertificateID),
+    constraint fk_MentorShip foreign key (MentorInstance,CertificateID) references TempCertificate (MechanicInstance,CertificateID),
+    constraint fk_Mentee foreign key(MenteeInstance) references Mechanic (MechanicInstances)
+);
+
+
 
 
