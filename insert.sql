f222b6486649b71b3cdc901de0004022381aa1bb
AlexTol <atol514@gmail.com>
2016-11-30 21:02:24 -0800

    fixed insert

--- insert.sql
+++ insert.sql
@@ -22,26 +22,26 @@ insert into Employee(EmployeeID, EFirstName, ELastName, Phone)
 values (10,'Derik','Derikson', '1111111119');
 
 
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#1
- values(1982,710-562-2011,0,'Bobsherman@hotmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#2
- Values(003,818-667-9479,0,'ealonzo003@gmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#3
- values(004,911-911-9111,0,'about4turtles@gmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#4
- values(21, 010-021-1997,0,'fake@Fakemail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#5
- values(57, 310-210-2879,0,'rocker@hooner.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#6
- values(5, 661-287-2882,0,'about5@gmail.com');
-insert into Customer(CustomerID,Phone,loyaltyPoints,email)#7
- values(109, 182-283-2918,0,'Customer@customermail.com');
-insert into customer(CustomerID,Phone,loyaltyPoints,email)#8
- values(1021,003-007-1021,0,'Fighter@jet.com');
-insert into customer(CustomerID,Phone,loyaltyPoints,email)#9
- values(1291,021-427-1231,0,'Matt@Matt.com');
- insert into customer(CustomerID,Phone,loyaltyPoints,email)#10
- values(1287,2153-237-2411,0,'Damn@Daniel.com');
+insert into Customer(CustomerID,Phone,email)#1
+ values(1982,710-562-2011,'Bobsherman@hotmail.com');
+insert into Customer(CustomerID,Phone,email)#2
+ Values(003,818-667-9479,'ealonzo003@gmail.com');
+insert into Customer(CustomerID,Phone,email)#3
+ values(004,911-911-9111,'about4turtles@gmail.com');
+insert into Customer(CustomerID,Phone,email)#4
+ values(21, 010-021-1997,'fake@Fakemail.com');
+insert into Customer(CustomerID,Phone,email)#5
+ values(57, 310-210-2879,'rocker@hooner.com');
+insert into Customer(CustomerID,Phone,email)#6
+ values(5, 661-287-2882,'about5@gmail.com');
+insert into Customer(CustomerID,Phone,email)#7
+ values(109, 182-283-2918,'Customer@customermail.com');
+insert into customer(CustomerID,Phone,email)#8
+ values(1021,003-007-1021,'Fighter@jet.com');
+insert into customer(CustomerID,Phone,email)#9
+ values(1291,021-427-1231,'Matt@Matt.com');
+ insert into customer(CustomerID,Phone,email)#10
+ values(1287,2153-237-2411,'Damn@Daniel.com');
 
  
  insert into Adress(CustomerID,AddressType,Street,AddressNumber,ZipCode)
