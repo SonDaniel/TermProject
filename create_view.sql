2e182532ccc0c2e166bd0b1c669a9a4fbb50b85e
Daniel Son <sondaniel96@gmail.com>
2016-12-03 12:41:45 -0800

    fixed customer_v view to have yearsJoined calculated

--- create_view.sql
+++ create_view.sql
@@ -1,10 +1,10 @@
 create view Customer_v as
-select FirstName, LastName, 'Prospective' as 'CustomerType' from individual inner join customer using(CustomerID) 
+select FirstName, LastName,(Year(curdate()) - DateJoined) as "YearsJoined", 'Prospective' as 'CustomerType' from individual inner join customer using(CustomerID) 
 inner join prospectivecustomer as P using(CustomerID)
 union
-select FirstName, LastName, 'Steady' as 'CustomerType' from individual inner join customer using(CustomerID)
+select FirstName, LastName, (Year(curdate()) - DateJoined) as "YearsJoined", 'Steady' as 'CustomerType' from individual inner join customer using(CustomerID)
 inner join contracted using(CustomerID) inner join steadycustomer using(CustomerID)
 union
-select FirstName, LastName, 'Premier' as 'CustomerType' from individual inner join customer using(CustomerID)
+select FirstName, LastName, (Year(curdate()) - DateJoined) as "YearsJoined",'Premier' as 'CustomerType' from individual inner join customer using(CustomerID)
 inner join contracted using(CustomerID) inner join premiumcustomer using(CustomerID);
 
