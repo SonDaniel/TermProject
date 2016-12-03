d0e42bd87d9eb96357c0d9b5ba76f287b4bbd9da
Daniel Son <sondaniel96@gmail.com>
2016-12-03 12:51:15 -0800

    added Customer_address View

--- create_view.sql
+++ create_view.sql
@@ -1,3 +1,4 @@
+--View for Customer
 create view Customer_v as
 select FirstName, LastName,(Year(curdate()) - DateJoined) as "YearsJoined", 'Prospective' as 'CustomerType' from individual inner join customer using(CustomerID) 
 inner join prospectivecustomer as P using(CustomerID)
@@ -8,3 +9,10 @@ union
 select FirstName, LastName, (Year(curdate()) - DateJoined) as "YearsJoined",'Premier' as 'CustomerType' from individual inner join customer using(CustomerID)
 inner join contracted using(CustomerID) inner join premiumcustomer using(CustomerID);
 
+--View for Customer_addresses
+create view Customer_addresses_v as
+select CustomerID ,'Corporate' as 'CustomerType', street, addressnumber, zipcode from corporation inner join customer using(CustomerID)
+inner join address using(CustomerID)
+union
+select CustomerID, 'Individual' as 'CustomerType', street, addressnumber, zipcode from individual inner join customer using(CustomerID)
+inner join address using(CustomerID);
