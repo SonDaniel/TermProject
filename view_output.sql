86ae8bacbaa952fbd9d901759fbbe2cfd2d53350
Daniel Son <sondaniel96@gmail.com>
2016-12-05 03:03:45 -0800

    added code for view output

--- view_output.sql
+++ view_output.sql
@@ -1,1 +1,8 @@
 #SQL file for displaying outputs
+select * from Customer_addresses_v;
+
+select * from Mechanic_mentor_v;
+
+select * from Premier_profits_v;
+
+select * from Prospective_resurrection_v;
