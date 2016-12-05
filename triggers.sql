493b564fb9e450adee824dd7af076aa4cb293d42
Jimmy <iamjimmyjimmy@yahoo.com>
2016-12-05 02:08:43 -0800

    one more

--- triggers.sql
+++ triggers.sql
@@ -39,7 +39,7 @@ begin
 	DECLARE n INT DEFAULT 0;
 	DECLARE cert INT DEFAULT 0;
     
-    DECLARE cur1 CURSOR FOR Select CertificateID FROM MaintenancePackage inner join servicepackageline
+    DECLARE cur1 CURSOR FOR Select CertificateID FROM MaintenancePackage inner join ServicePackageLine
 								on (MaintenancePackage.MaintenancePackageID = ServicePackageLine.MaintenancePackageID)
 								inner join ServiceItem on (ServicePackageLine.ServiceitemID = ServiceItem.ServiceitemID)
 								inner join IndividualService on (ServiceItem.ServiceitemID = IndividualService.ServiceitemID)
